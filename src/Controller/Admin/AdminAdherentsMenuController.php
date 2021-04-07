1e139c36d81371b6aac5bd1e1fcc383afd4d0734
Marjorie Meziere <mmwebforce20@gmail.com>
2021-04-07 22:30:03 +0200

    menu adherents en cours

---
+++ src/Controller/Admin/AdminAdherentsMenuController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminAdherentsMenuController extends AbstractController
+{
+    #[Route('/admin/adherents', name: 'menu-adherents')]
+    public function index(): Response
+    {
+        return $this->render('admin_adherents_menu/index.html.twig', [
+            'controller_name' => 'AdminAdherentsMenuController',
+        ]);
+    }
+}
