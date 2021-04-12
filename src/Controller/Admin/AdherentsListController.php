d33c3b7e14ad25e5f020c5154ad2f768c5d23af8
Marjorie Meziere <mmwebforce20@gmail.com>
2021-04-12 23:02:43 +0200

    Création du tableau des adhérents et récupération des données

--- src/Controller/Admin/AdherentsListController.php
+++ src/Controller/Admin/AdherentsListController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Repository\AdherentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,12 +10,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdherentsListController extends AbstractController
 {
     #[Route('/admin/adherents/list', name: 'admin_adherents_list')]
-    public function index(): Response
+    public function index(AdherentRepository $repo): Response
     {
+        $adherents = $repo->findAll();
+
         return $this->render('admin/adherents_list/index.html.twig', [
             'controller_name' => 'AdherentsListController',
+            'adherents' => $adherents,
             'section' => 'section-adherent',
-            'return_path' => 'menu-adherent'
+            'return_path' => 'menu-adherent',
+            'color' => 'adherent-color'
         ]);
     }
 }
