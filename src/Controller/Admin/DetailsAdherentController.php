82f9c8a834c1bdd53154f3915ef50d53ddaa93a8
Marjorie Meziere <mmwebforce20@gmail.com>
2021-04-16 20:20:12 +0200

    ajout page des détails d'un adhérent et refactorisation

---
+++ src/Controller/Admin/DetailsAdherentController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Repository\AdherentRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class DetailsAdherentController extends AbstractController
+{
+    #[Route('/admin/details/adherent/{slug}', name: 'admin_details_adherent')]
+    public function index($slug, AdherentRepository $adherentRepository): Response
+
+    {
+        $adherent = $adherentRepository->findOneBySlug($slug);
+        dump($slug);
+
+
+        return $this->render('admin/pages_details/details_adherent.html.twig', [
+            'controller_name' => 'DetailsAdherentController',
+            'adherent' => $adherent,
+            'return_path' => 'menu-adherent',
+            'section' => 'section-adherents',
+            'color' => 'adherents-color'
+        ]);
+    }
+}
