e7a7af3f38490893838c24be2733a225e3063478
Marjorie Meziere <mmwebforce20@gmail.com>
2021-04-21 11:01:23 +0200

    ajout fontawesome

--- assets/app.js
+++ assets/app.js
@@ -5,7 +5,8 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
 
 // any CSS you import will output into a single css file (app.css in this case)
 import "./styles/app.sass";
