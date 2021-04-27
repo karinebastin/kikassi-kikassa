3d69c918117ceae2fa7751dc7ab18e9e1345f220
karinebastin <karine-bastin@orange.fr>
2021-04-27 09:18:28 +0200

    Affichage en français

--- assets/calendrier.js
+++ assets/calendrier.js
@@ -11,68 +11,28 @@ document.addEventListener("DOMContentLoaded", function () {
   var calendar = new Calendar(calendarEl, {
     plugins: [interactionPlugin, dayGridPlugin, timeGridPlugin, listPlugin],
     headerToolbar: {
-      left: "today prev,next",
+      left: "prev,next",
       center: "title",
       right: "dayGridMonth,listWeek",
     },
-    initialDate: "2018-01-12",
+    buttonText: {
+      today: "Aujourd'hui",
+      month: "mois",
+      week: "semaine",
+      day: "jour",
+      list: "liste",
+    },
+    initialDate: Date.now(),
+    locale: "fr",
+    firstDay: "1",
     navLinks: true, // can click day/week names to navigate views
     editable: true,
     dayMaxEvents: true, // allow "more" link when too many events
     events: [
       {
-        title: "All Day Event",
-        start: "2018-01-01",
-      },
-      {
         title: "Long Event",
-        start: "2018-01-07",
-        end: "2018-01-10",
-      },
-      {
-        groupId: 999,
-        title: "Repeating Event",
-        start: "2018-01-09T16:00:00",
-      },
-      {
-        groupId: 999,
-        title: "Repeating Event",
-        start: "2018-01-16T16:00:00",
-      },
-      {
-        title: "Conference",
-        start: "2018-01-11",
-        end: "2018-01-13",
-      },
-      {
-        title: "Meeting",
-        start: "2018-01-12T10:30:00",
-        end: "2018-01-12T12:30:00",
-      },
-      {
-        title: "Lunch",
-        start: "2018-01-12T12:00:00",
-      },
-      {
-        title: "Meeting",
-        start: "2018-01-12T14:30:00",
-      },
-      {
-        title: "Happy Hour",
-        start: "2018-01-12T17:30:00",
-      },
-      {
-        title: "Dinner",
-        start: "2018-01-12T20:00:00",
-      },
-      {
-        title: "Birthday Party",
-        start: "2018-01-13T07:00:00",
-      },
-      {
-        title: "Click for Google",
-        url: "http://google.com/",
-        start: "2018-01-28",
+        start: "2021-04-01",
+        end: "2021-04-14",
       },
     ],
   });
