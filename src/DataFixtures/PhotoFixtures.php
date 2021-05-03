64f1ba42c256e073f83cb62e7bad8b3f2c9a0f67
Marjorie Meziere <mmwebforce20@gmail.com>
2021-05-03 17:29:33 +0200

    récupération fichiers supprimés par erreur

---
+++ src/DataFixtures/PhotoFixtures.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Photo;
+use App\DataFixtures\ObjetFixtures;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+
+class PhotoFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $liens = [
+            'marteau.jpg',
+            'perceuse.jpg',
+            'aspirateur.jpg',
+            'casseroles.jpg',
+            'robot.jpg',
+            'livre.jpg',
+            'jouet.jpg',
+            'tondeuse.jpg',
+        ];
+
+        for ($i = 0; $i < 8; $i++) {
+            $objet = $this->getReference('objet_' . $i);
+            $photo = new Photo();
+
+            $photo->setLien($liens[$i])->setObjet($objet);
+
+            $manager->persist($photo);
+        }
+
+        $manager->flush();
+    }
+    public function getDependencies()
+    {
+        return [ObjetFixtures::class];
+    }
+}
