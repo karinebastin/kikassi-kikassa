308751dd377192fb59a8001cf8b8091efeed48ca
t'eshaut <stivaltheo@gmail.com>
2021-05-11 09:38:57 +0200

    déploim0ent des catégorie catalogue

--- assets/catalogue.js
+++ assets/catalogue.js
@@ -1,81 +1,88 @@
-// import Splide from "@splidejs/splide";
-// document.addEventListener("DOMContentLoaded", function () {
-//   var secondarySlider = new Splide("#secondary-slider", {
-//     fixedWidth: 100,
-//     height: 60,
-//     gap: 10,
-//     cover: true,
-//     isNavigation: true,
-//     focus: "center",
-//     breakpoints: {
-//       600: {
-//         fixedWidth: 66,
-//         height: 40,
-//       },
-//     },
-//   }).mount();
+// Listen for click on the document
+document.addEventListener('click', function (event) {
+  
+  //Bail if our clicked element doesn't have the class
+  if (!event.target.classList.contains('accordion-toggle')) return;
+  
+  // Get the target content
+  var content = document.querySelector(event.target.hash);
+  if (!content) return;
+  
+  // Prevent default link behavior
+  event.preventDefault();
+  
+  // If the content is already expanded, collapse it and quit
+  if (content.classList.contains('active')) {
+    content.classList.remove('active');
+    return;
+  }
+  
+  // Get all open accordion content, loop through it, and close it
+  var accordions = document.querySelectorAll('.accordion-content.active');
+  for (var i = 0; i < accordions.length; i++) {
+    accordions[i].classList.remove('active');
+  }
+  
+  // Toggle our content
+  content.classList.toggle('active');
+})
 
-//   var primarySlider = new Splide("#primary-slider", {
-//     type: "fade",
-//     heightRatio: 0.5,
-//     pagination: false,
-//     arrows: false,
-//     cover: true,
-//   });
 
-//   primarySlider.sync(secondarySlider).mount();
-// });
+// isotop
 
-// document.addEventListener( 'DOMContentLoaded', function () {
-// 	new Splide( '#third-slider', {
-//     fixedWidth : 200,
-// 		height     : 200,
-// 		gap        : 10,
-// 		rewind     : true,
-// 		cover      : true,
-// 		pagination : false,
-// 	} ).mount();
-// } );
-	
-			
-// document.addEventListener( 'DOMContentLoaded', function () {
-// 	new Splide( '#fourth-slider', {
-// 		fixedWidth : 200,
-// 		height     : 200,
-// 		gap        : 10,
-// 		rewind     : true,
-// 		cover      : true,
-// 		pagination : false,
-// 	} ).mount();
-// } );
+// external js: isotope.pkgd.js
 
- //accordéon
 
-  //Listen for click on the document
- document.addEventListener('click', function (event) {
+// init Isotope
+var $grid = $('.grid').isotope({
+  itemSelector: '.element-item',
+  layoutMode: 'fitRows',
+  getSortData: {
+    name: '.name',
+    symbol: '.symbol',
+    number: '.number parseInt',
+    category: '[data-category]',
+    weight: function( itemElem ) {
+      var weight = $( itemElem ).find('.weight').text();
+      return parseFloat( weight.replace( /[\(\)]/g, '') );
+    }
+  }
+});
+
+// filter functions
+var filterFns = {
+  // show if number is greater than 50
+  numberGreaterThan50: function() {
+    var number = $(this).find('.number').text();
+    return parseInt( number, 10 ) > 50;
+  },
+  // show if name ends with -ium
+  ium: function() {
+    var name = $(this).find('.name').text();
+    return name.match( /ium$/ );
+  }
+};
+
+// bind filter button click
+$('#filters').on( 'click', 'button', function() {
+  var filterValue = $( this ).attr('data-filter');
+  // use filterFn if matches value
+  filterValue = filterFns[ filterValue ] || filterValue;
+  $grid.isotope({ filter: filterValue });
+});
+
+// bind sort button click
+$('#sorts').on( 'click', 'button', function() {
+  var sortByValue = $(this).attr('data-sort-by');
+  $grid.isotope({ sortBy: sortByValue });
+});
+
+// change is-checked class on buttons
+$('.button-group').each( function( i, buttonGroup ) {
+  var $buttonGroup = $( buttonGroup );
+  $buttonGroup.on( 'click', 'button', function() {
+    $buttonGroup.find('.is-checked').removeClass('is-checked');
+    $( this ).addClass('is-checked');
+  });
+});
   
-   //Bail if our clicked element doesn't have the class
-   if (!event.target.classList.contains('accordion-toggle')) return;
-  
-   // Get the target content
-   var content = document.querySelector(event.target.hash);
-   if (!content) return;
-  
-   // Prevent default link behavior
-   event.preventDefault();
-  
-   // If the content is already expanded, collapse it and quit
-   if (content.classList.contains('active')) {
-     content.classList.remove('active');
-     return;
-   }
-  
-   // Get all open accordion content, loop through it, and close it
-   var accordions = document.querySelectorAll('.accordion-content.active');
-   for (var i = 0; i < accordions.length; i++) {
-     accordions[i].classList.remove('active');
-   }
-  
-   // Toggle our content
-   content.classList.toggle('active');
- })
