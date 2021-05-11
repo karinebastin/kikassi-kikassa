66a537fec5ce9871027aa3d14be71c6ed5a53ca7
karinebastin <karine-bastin@orange.fr>
2021-05-11 10:30:49 +0200

    changement avant le 11 mai

--- src/Form/EmpruntType.php
+++ src/Form/EmpruntType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 
 class EmpruntType extends AbstractType
 {
@@ -15,10 +16,14 @@ class EmpruntType extends AbstractType
         $builder
             ->add('date_debut', DateType::class, [
                 'widget' => 'single_text',
+                'placeholder' => ['Date de début au format 01/01/2000']
             ])
             ->add('date_fin', DateType::class, [
                 'widget' => 'single_text',
-
+                'placeholder' => ['Date de fin au format 01/01/2000']
+            ])
+            ->add('emprunt_regle', HiddenType::class, [
+                'data' => 'false'
             ]);
     }
 
